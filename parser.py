f217fb12114687f0cb29cf53125d8855286f702f
unknown <tomasko.zgut@gmail.com>
2023-01-31 19:32:55 +0100

    added a py parser

---
+++ parser.py
@@ -0,0 +1,49 @@
+import json
+import sys
+
+
+def load_lines():
+    out_lst = []
+    for line in sys.stdin:
+        if 'Exit' == line.rstrip():
+            break
+        out_lst.append(line)
+    return out_lst
+
+def transfrom_data(data):
+    out_lst = []
+    for line in data:
+        line = line.split(',')
+        line_dct = {
+            'clues': line[:5],
+            'answer': line[5],
+            'translate': line[6].rstrip('\n')
+        }
+
+        out_lst.append(line_dct)
+
+    return out_lst
+
+def update_json_file(updatedJsonString):
+        with open('cards.JSON','r') as f_old:
+            oldJsonString = f_old.read()
+
+        if oldJsonString == '':
+            newJsonString = updatedJsonString
+        else :
+            newJsonString = f"{oldJsonString[:-2]},\n{updatedJsonString[3:]}"
+
+        with open("cards.JSON","w") as f_new:
+            f_new.write(newJsonString)
+
+def main():
+    lines = load_lines()
+
+    data_dct = transfrom_data(lines)
+    addedJsonString = json.dumps(data_dct,indent=4)
+
+    update_json_file(addedJsonString)
+
+
+if __name__ == '__main__':
+    main()
