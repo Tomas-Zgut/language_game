646bd8660c9d01a7bf26bab45c7e2916b68a5a3b
Tomas-Zgut <tomasko.zgut@gmail.com>
2023-02-04 19:08:49 +0100

    made the parser use the cmd args (optionally)

--- parser_src/parser.py
+++ parser_src/parser.py
@@ -1,20 +1,35 @@
 import json
+import argparse
 
+def parse_aguments():
+    ap = argparse.ArgumentParser()
 
-def load_lines():
-   with open('new.txt',encoding="UTF-8") as inptf:
-    out = inptf.read()
-    out_lst = out.splitlines()
-    return out_lst
+    default_inpf = 'new.txt'
+    default_delim = ','
 
-def transfrom_data(data):
+    ap.add_argument('-i','--inpfilename', default = default_inpf,
+    help=f'Specify a different inpt fille from the deafult "{default_inpf}"')
+
+    ap.add_argument('-d','--delimiter', default = default_delim,
+    help = f'Changes the delimiter in input file from the default "{default_delim}"; use "" to input the delimiter')
+
+    return ap.parse_args()
+
+def load_lines(input_file):
+    with open(input_file,encoding="UTF-8") as inptf:
+        out = inptf.read()
+        out_lst = out.splitlines()
+        return out_lst
+
+def transfrom_data(data,delimiter):
     out_lst = []
     for line in data:
-        line = line.split(',')
+        line = line.split(delimiter)
+        print(line)
+
         line_dct = {
             'clues': line[:5],
-            'answer': line[5],
-            'translate': line[6].rstrip('\n')
+            'answer': line[5].rstrip('\n')
         }
 
         out_lst.append(line_dct)
@@ -32,23 +47,27 @@ def update_json_file(updatedJsonString):
 
         with open('page_src/js/cards.JSON','w') as f_new:
             f_new.write(newJsonString)
-def clear_input():
-    with open('new.txt','w') as inpf:
+
+
+def clear_input(input_file):
+    with open(input_file,'w') as inpf:
         inpf.write('')
 
 def main():
-    lines = load_lines()
+    agrs = parse_aguments()
+
+    lines = load_lines(agrs.inpfilename)
 
     if lines == []:
-        print('Use the "new" file!')
+        print('The input file is empty!')
         return
         
-    data_dct = transfrom_data(lines)
+    data_dct = transfrom_data(lines,agrs.delimiter)
     addedJsonString = json.dumps(data_dct,indent=4)
 
     update_json_file(addedJsonString)
 
-    clear_input()
+    clear_input(agrs.inpfilename)
 
 
 if __name__ == '__main__':
