af48ce5513bc5651adb379c87024ff448a038a9b
Tomas-Zgut <tomasko.zgut@gmail.com>
2023-02-24 15:44:26 +0100

    added a file with custom errors

---
+++ adder_src/custom_errors.py
@@ -0,0 +1,7 @@
+class DelimiterNotFoundError(Exception):
+    """Exception to be thrown when a specified delimiter is not found"""
+    pass
+
+class FileEmptyError(Exception):
+    """Exception to be thrown when a file is empty"""
+    pass
